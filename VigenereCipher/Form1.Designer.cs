2d6d74d5d8f32ed231c2cc92e13a5bb3b05a5520
MrChri5 <chrisjweaver@outlook.com>
2022-05-19 06:25:06 +0100

    Add project files.

---
+++ VigenereCipher/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace VigenereCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.LoadFileBtn = new System.Windows.Forms.Button();
+            this.SaveFileBtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.EncodeBtn = new System.Windows.Forms.Button();
+            this.DecodeBtn = new System.Windows.Forms.Button();
+            this.lcm = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.msgLengthLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.keywordList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cipherFormat = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.InputBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputBox.Location = new System.Drawing.Point(0, 0);
+            this.InputBox.Margin = new System.Windows.Forms.Padding(4);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputBox.Size = new System.Drawing.Size(670, 600);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.TextChanged += new System.EventHandler(this.CalculateStats);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputBox.Location = new System.Drawing.Point(678, 0);
+            this.OutputBox.Margin = new System.Windows.Forms.Padding(4);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.ReadOnly = true;
+            this.OutputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputBox.Size = new System.Drawing.Size(664, 600);
+            this.OutputBox.TabIndex = 7;
+            // 
+            // LoadFileBtn
+            // 
+            this.LoadFileBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadFileBtn.Location = new System.Drawing.Point(983, 792);
+            this.LoadFileBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.LoadFileBtn.Name = "LoadFileBtn";
+            this.LoadFileBtn.Size = new System.Drawing.Size(119, 50);
+            this.LoadFileBtn.TabIndex = 5;
+            this.LoadFileBtn.Text = "Load File";
+            this.LoadFileBtn.UseVisualStyleBackColor = true;
+            this.LoadFileBtn.Click += new System.EventHandler(this.LoadFileBtn_Click);
+            // 
+            // SaveFileBtn
+            // 
+            this.SaveFileBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveFileBtn.Location = new System.Drawing.Point(983, 849);
+            this.SaveFileBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SaveFileBtn.Name = "SaveFileBtn";
+            this.SaveFileBtn.Size = new System.Drawing.Size(119, 50);
+            this.SaveFileBtn.TabIndex = 6;
+            this.SaveFileBtn.Text = "Save File";
+            this.SaveFileBtn.UseVisualStyleBackColor = true;
+            this.SaveFileBtn.Click += new System.EventHandler(this.SaveFileBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "test message.txt";
+            // 
+            // EncodeBtn
+            // 
+            this.EncodeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.EncodeBtn.Location = new System.Drawing.Point(856, 792);
+            this.EncodeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.EncodeBtn.Name = "EncodeBtn";
+            this.EncodeBtn.Size = new System.Drawing.Size(119, 50);
+            this.EncodeBtn.TabIndex = 3;
+            this.EncodeBtn.Text = "Encode";
+            this.EncodeBtn.UseVisualStyleBackColor = true;
+            this.EncodeBtn.Click += new System.EventHandler(this.EncodeBtn_Click);
+            // 
+            // DecodeBtn
+            // 
+            this.DecodeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.DecodeBtn.Location = new System.Drawing.Point(856, 849);
+            this.DecodeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.DecodeBtn.Name = "DecodeBtn";
+            this.DecodeBtn.Size = new System.Drawing.Size(119, 50);
+            this.DecodeBtn.TabIndex = 4;
+            this.DecodeBtn.Text = "Decode";
+            this.DecodeBtn.UseVisualStyleBackColor = true;
+            this.DecodeBtn.Click += new System.EventHandler(this.DecodeBtn_Click);
+            // 
+            // lcm
+            // 
+            this.lcm.AutoSize = true;
+            this.lcm.Location = new System.Drawing.Point(262, 31);
+            this.lcm.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lcm.Name = "lcm";
+            this.lcm.Size = new System.Drawing.Size(23, 25);
+            this.lcm.TabIndex = 10;
+            this.lcm.Text = "1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.Maroon;
+            this.progressBar1.Location = new System.Drawing.Point(9, 59);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(246, 33);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 11;
+            // 
+            // msgLengthLbl
+            // 
+            this.msgLengthLbl.AutoSize = true;
+            this.msgLengthLbl.Location = new System.Drawing.Point(262, 68);
+            this.msgLengthLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.msgLengthLbl.Name = "msgLengthLbl";
+            this.msgLengthLbl.Size = new System.Drawing.Size(23, 25);
+            this.msgLengthLbl.TabIndex = 12;
+            this.msgLengthLbl.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Cipher Strength:";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.InputBox);
+            this.panel1.Controls.Add(this.OutputBox);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1346, 604);
+            this.panel1.TabIndex = 14;
+            // 
+            // keywordList
+            // 
+            this.keywordList.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.keywordList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.keywordList.BackColor = System.Drawing.SystemColors.Window;
+            this.keywordList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.keywordList.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.keywordList.GridLines = true;
+            this.keywordList.HideSelection = false;
+            this.keywordList.LabelEdit = true;
+            this.keywordList.Location = new System.Drawing.Point(0, 613);
+            this.keywordList.Margin = new System.Windows.Forms.Padding(4);
+            this.keywordList.Name = "keywordList";
+            this.keywordList.Size = new System.Drawing.Size(670, 399);
+            this.keywordList.TabIndex = 14;
+            this.keywordList.UseCompatibleStateImageBehavior = false;
+            this.keywordList.View = System.Windows.Forms.View.Details;
+            this.keywordList.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.keywordList_AfterLabelEdit);
+            this.keywordList.BeforeLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.keywordList_BeforeLabelEdit);
+            this.keywordList.ItemActivate += new System.EventHandler(this.keywordList_ItemActivate);
+            this.keywordList.SelectedIndexChanged += new System.EventHandler(this.keywordList_SelectedIndexChanged);
+            this.keywordList.Leave += new System.EventHandler(this.keywordList_SelectedIndexChanged);
+            this.keywordList.Validating += new System.ComponentModel.CancelEventHandler(this.keywordList_Validating);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Keyword";
+            this.columnHeader1.Width = 250;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Length";
+            this.columnHeader2.Width = 100;
+            // 
+            // cipherFormat
+            // 
+            this.cipherFormat.AutoSize = true;
+            this.cipherFormat.Location = new System.Drawing.Point(62, 96);
+            this.cipherFormat.Margin = new System.Windows.Forms.Padding(6);
+            this.cipherFormat.Name = "cipherFormat";
+            this.cipherFormat.Size = new System.Drawing.Size(193, 29);
+            this.cipherFormat.TabIndex = 15;
+            this.cipherFormat.Text = "Ciphertext Format";
+            this.cipherFormat.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.cipherFormat);
+            this.groupBox1.Controls.Add(this.lcm);
+            this.groupBox1.Controls.Add(this.msgLengthLbl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(816, 641);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(301, 142);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 1036);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.keywordList);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DecodeBtn);
+            this.Controls.Add(this.EncodeBtn);
+            this.Controls.Add(this.SaveFileBtn);
+            this.Controls.Add(this.LoadFileBtn);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(1355, 1054);
+            this.Name = "Form1";
+            this.Text = "Vigenere Cipher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBox;       
+        private System.Windows.Forms.Button LoadFileBtn;
+        private System.Windows.Forms.Button SaveFileBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button EncodeBtn;
+        private System.Windows.Forms.Button DecodeBtn;
+        private System.Windows.Forms.Label lcm;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label msgLengthLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView keywordList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.CheckBox cipherFormat;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ VigenereCipher/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VigenereCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] autoAddText = { "_______", "" };
+        bool editing = false;
+
+        //arbitrary max number of keywords
+        int noKeys = 10;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // auto-add item
+            keywordList.Items.Clear();
+            ListViewItem autoAdd = new ListViewItem(autoAddText);
+            keywordList.Items.Add(autoAdd);
+        }
+
+        private void keywordList_ItemActivate(object sender, EventArgs e)
+        {
+            //set activated item to edit mode
+            if (keywordList.SelectedItems.Count == 1)
+            {
+                keywordList.SelectedItems[0].BeginEdit();
+            }
+        }
+
+        private void keywordList_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            //add new auto-add row if last row was edited
+            editing = false;
+            if (e.Label != null && keywordList.SelectedIndices[0] == keywordList.Items.Count - 1)
+            {
+                ListViewItem autoAdd = new ListViewItem(autoAddText);
+                keywordList.Items.Add(autoAdd);                
+            }
+        }
+
+        private void keywordList_BeforeLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            // set editing flag to prevent focus leaving listview before formatting
+            editing = true;
+        }
+
+        private void keywordList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!editing)
+            {
+
+                for (int i = 0; i < keywordList.Items.Count - 1; i++)
+                {
+                    //format each entry to remove special characters
+                    string newText = KeywordEdit(keywordList.Items[i].Text);
+                    if (newText == "")
+                    {
+                        //remove any blank items, except the auto-add row
+                        keywordList.Items.RemoveAt(i);
+                    }
+                    else if (newText != keywordList.Items[i].Text)
+                    {
+                        //Add length as subitem
+                        string[] newItemText = { newText, newText.Length.ToString() };
+                        ListViewItem newItem = new ListViewItem(newItemText);
+                        newItem.Selected = keywordList.Items[i].Selected;
+                        keywordList.Items[i] = newItem;
+                    }
+                }
+                //redisplay cipher strength
+                CalculateStats();
+            }
+        }
+
+        private void keywordList_Validating(object sender, CancelEventArgs e)
+        {
+            //if editing don't leave the listview
+            e.Cancel = editing;
+        }
+
+        private void LoadFileBtn_Click(object sender, EventArgs e)
+        {
+            //load .txt file into input textbox        
+            string openFilePath = "";
+            openFileDialog1.InitialDirectory = "C:\\Users\\chris\\OneDrive\\VSProjects\\VigenereCipher";
+            openFileDialog1.Filter = "Text file|*.txt";
+            openFileDialog1.Title = "Open text file";
+
+            if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                openFilePath = openFileDialog1.FileName;
+                System.IO.StreamReader streamReader = new System.IO.StreamReader(openFilePath);
+                InputBox.Text = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+        }
+
+        private void SaveFileBtn_Click(object sender, EventArgs e)
+        {
+            //save output textbox to .txt file
+            string saveFilePath = "";
+            saveFileDialog1.InitialDirectory = "C:\\Users\\chris\\OneDrive\\VSProjects\\VigenereCipher";
+            saveFileDialog1.Filter = "Text file|*.txt";
+            saveFileDialog1.Title = "Save text file";
+
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                saveFilePath = saveFileDialog1.FileName;
+                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(saveFilePath);
+                streamWriter.WriteLine(OutputBox.Text);
+                streamWriter.Close();
+            }
+        }
+
+        private string KeywordEdit(string inputKeyword)
+        {
+            //remove any special characters and convert to upper
+            string newKeyword = "";
+            for (int i = 0; i < inputKeyword.Length; i++)
+            {
+                if (inputKeyword[i] >= 'A' && inputKeyword[i] <= 'Z' || inputKeyword[i] >= 'a' && inputKeyword[i] <= 'z')
+                {
+                    newKeyword += inputKeyword[i];
+                }
+            }
+            return newKeyword.ToUpper();
+        }
+
+        private void CalculateStats(object sender, EventArgs e)
+        {
+            CalculateStats();
+        }
+        private void CalculateStats()
+        {
+            int[] keyLengths = new int[noKeys];
+            //read lengths of keywords from list
+            for(int i = 0;i < keyLengths.Length; i++)
+            {
+                if (i < keywordList.Items.Count && keywordList.Items[i].Text != autoAddText[0])
+                {
+                    keyLengths[i] = keywordList.Items[i].Text.Length;
+                }
+                //for lcm calculations, length 0 has lcm 1
+                if (keyLengths[i] == 0)
+                {
+                    keyLengths[i] = 1;
+                }
+            }
+            //cipher strength = lcm of all keyword lengths
+            int cipherStrength = Lcm(keyLengths);
+            lcm.Text = cipherStrength.ToString();
+            //calculate number of letters in message
+            //N.B cipher replaces all other characters with space and doesn't format into blocks 
+            int msgLength = 0;
+            for (int i = 0; i < InputBox.TextLength; i++)
+            {
+                if (InputBox.Text[i] >= 'A' && InputBox.Text[i] <= 'Z')
+                {
+                    msgLength++;
+                }
+            }
+            msgLengthLbl.Text = msgLength.ToString();
+            //display cipher strength vs message length as a progress bar
+            progressBar1.Value = 0;
+            if (msgLength != 0)
+            {
+                progressBar1.Value = Math.Min(progressBar1.Maximum, (100 * cipherStrength) / msgLength);
+            }                               
+        }
+
+        private int Lcm(int[] xArray)
+        {
+            //lcm calculations for many numbers 
+            int lcmValue = 1;
+            for (int i = 0; i < xArray.Length; i++)
+            {
+                lcmValue = Lcm(lcmValue, xArray[i]);
+            }
+            return lcmValue;
+        }
+        private int Lcm(int x, int y)
+        {
+            //take abs of values
+            if (x < 0)
+            {
+                x *= -1;
+            }
+            if (y < 0)
+            {
+                y *= -1;
+            }
+
+            //lcm for two positve integers
+            int lcmValue = x * y;
+            int xRem;
+            int yRem;
+
+            for (int i = 2; i <= x && i <= y;)
+            {
+                Math.DivRem(x, i, out xRem);
+                Math.DivRem(y, i, out yRem);
+                if (xRem == 0 && yRem == 0)
+                {
+                    x /= i;
+                    y /= i;
+                    lcmValue /= i;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return lcmValue;
+        }
+
+        private void DecodeBtn_Click(object sender, EventArgs e)
+        {
+            encode(false);
+        }
+
+        private void EncodeBtn_Click(object sender, EventArgs e)
+        {
+            encode(true);
+        }
+
+        private void encode(bool encodeDecode)
+        {
+            //encode or decode message
+            int encodePtr = -1;
+            if (encodeDecode)
+            {
+                encodePtr = 1;
+            }
+            string messageText = InputBox.Text;
+            string cipherText = "";      
+            string[] keys = new string[noKeys];
+            int[] keyCounters = new int[noKeys];
+            //read keys and initialize key counters
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (i < keywordList.Items.Count)
+                {
+                    keys[i] = KeywordEdit(keywordList.Items[i].Text);
+                    keyCounters[i] = 0;
+                }
+                //empty keys are replaced with identity key 'Z'
+                if (keys[i] == "" || keys[i] == null)
+                {                  
+                    keys[i] = "Z";
+                }
+            }
+            int cipherLetterValue = 0;
+            int cipherFormatPtr = 0;
+            foreach (char msgLetter in messageText)
+            {
+                //skip special characters and replace with spaces
+                if (msgLetter < 'A' || msgLetter > 'Z')
+                {
+                    if (!cipherFormat.Checked || !encodeDecode)
+                    {
+                        cipherText += " ";
+                    }
+                    continue;
+                }
+                cipherLetterValue = (int)msgLetter;
+                //add keyword letters to message letter to obtain cipher letter or reverse if decoding
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    cipherLetterValue += (int)encodePtr * ((keys[i])[keyCounters[i]] - 'A' + 1);
+                    keyCounters[i]++;
+                    if (keyCounters[i] == keys[i].Length)
+                    {
+                        keyCounters[i] = 0;
+                    }
+                }
+                //bring char value back into uppercase letter range
+                while (cipherLetterValue < (int)'A' || cipherLetterValue > (int)'Z')
+                {
+                    cipherLetterValue -= encodePtr * 26; // would not work in EBCDIC as A-Z is not continuous
+                }
+                cipherText += ((char)cipherLetterValue).ToString();
+                // Add a space every five letters if Ciphertext Format is checked
+                ++cipherFormatPtr;
+                if (cipherFormatPtr == 5 && cipherFormat.Checked && encodeDecode)
+                {
+                    cipherText += " ";
+                    cipherFormatPtr = 0;
+                }
+            }
+            //if using Ciphertext format, add characters to make up final block
+            if(cipherFormat.Checked && encodeDecode && cipherFormatPtr !=0)
+            {
+                while(cipherFormatPtr < 5)
+                {
+                    cipherText += "X";
+                    ++cipherFormatPtr;
+                }
+            }
+
+            //display message in output textbox 
+            OutputBox.Text = cipherText;
+        }
+    }
+}
